d78cb641f409e79edd565f827fd9f8cdb9f1092b
affanyunas <yunas.affan@gmail.com>
2024-12-25 10:12:49 +0700

    Remove unused backend and frontend files, including environment configurations, Dockerfiles, and source code

--- .idx/dev.nix
+++ .idx/dev.nix
@@ -1,50 +1,34 @@
-# tom-server/dev.nix
+# To learn more about how to use Nix to configure your environment
+# see: https://developers.google.com/idx/guides/customize-idx-env
 { pkgs, ... }: {
-  channel = "stable-24.05";
-  
+  # Which nixpkgs channel to use.
+  channel = "stable-24.05"; # or "unstable"
+  # Use https://search.nixos.org/packages to find packages
   packages = [
-    # Frontend dependencies
-    pkgs.nodejs_20
-    pkgs.yarn
-    
-    # Backend dependencies
     pkgs.cargo
     pkgs.rustc
     pkgs.rustfmt
     pkgs.stdenv.cc
   ];
-  
+  # Sets environment variables in the workspace
   env = {
     RUST_SRC_PATH = "${pkgs.rustPlatform.rustLibSrc}";
   };
-  
   idx = {
+    # Search for the extensions you want on https://open-vsx.org/ and use "publisher.id"
     extensions = [
-      # Backend extensions
       "rust-lang.rust-analyzer"
       "tamasfe.even-better-toml"
       "serayuzgur.crates"
       "vadimcn.vscode-lldb"
     ];
-    
     workspace = {
       onCreate = {
-        npm-install = "cd frontend && npm ci --no-audit --prefer-offline --no-progress --timing";
-        default.openFiles = [
-          "frontend/src/App.tsx"
-          "backend/src/main.rs"
-        ];
+        # Open editors for the following files by default, if they exist:
+        default.openFiles = ["src/main.rs"];
       };
     };
-    
-    previews = {
-      enable = true;
-      previews = {
-        frontend = {
-          command = ["cd" "frontend" "&&" "npm" "run" "dev" "--" "--port" "$PORT" "--host" "0.0.0.0"];
-          manager = "web";
-        };
-      };
-    };
+    # Enable previews and customize configuration
+    previews = {};
   };
 }
